5bd5c35fb8d5296876f03c5861fd97a5e473ffb0
davit hermansyah <davit@logique.co.id>
2022-10-02 14:14:51 +0700

    test mock

--- src/ProductRepository.php
+++ src/ProductRepository.php
@@ -4,7 +4,7 @@ namespace Davit37\PhpUnit;
 interface ProductRepository 
 {
     public function save(Product $product): Product;
-    public function delete(Product $product): void;
+    public function delete(?Product $product): void;
     public function findById(string $id): ?Product;
     public function findAll(): array;
 }
--- tests/ProductServiceMockTest.php
+++ tests/ProductServiceMockTest.php
@@ -5,14 +5,14 @@ namespace Davit37\PhpUnit;
 use PHPUnit\Framework\TestCase;
 
 
-class ProductServiceTest extends TestCase
+class ProductServiceMockTest extends TestCase
 {
     private ProductRepository $repository;
     private ProductService $service;
 
     protected function setUp():void 
     {
-        $this->repository = $this->createStub(ProductRepository::class);
+        $this->repository = $this->createMock(ProductRepository::class);
         $this->service = new ProductService($this->repository);
     }
 
@@ -94,6 +94,9 @@ class ProductServiceTest extends TestCase
 
     public function testDelete()
     {
+        $this->repository->expects($this->once())
+        ->method("delete");
+
         $product = new Product();
         $product->setId("1");
 
@@ -106,6 +109,9 @@ class ProductServiceTest extends TestCase
 
     public function testDeleteFailed()
     {
+        $this->repository->expects($this->never())
+        ->method("delete");
+
         $this->expectException(\Exception::class);
         $this->repository->method("findById")
         ->willReturn(null);
@@ -116,9 +122,25 @@ class ProductServiceTest extends TestCase
 
     public function testDeleteException() 
     {
+        $this->repository->expects($this->never())
+        ->method("delete");
+        
         $this->expectException(\Exception::class);
         $this->repository->method("findById")->willReturn(null);
 
         $this->service->delete("1");
+    }
+
+    public function testMock()
+    {
+        $product = new Product();
+        $product->setId("1");
+
+        $this->repository->expects($this->once())
+        ->method("findById")
+        ->willReturn($product);
+
+        $result = $this->repository->findById("1");
+        self::assertSame($product, $result);
     }
 }
